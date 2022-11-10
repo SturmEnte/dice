e8d08530d4205f0ce9f09283da98d75fbfb32bae
SturmEnte <sturmente@proton.me>
2022-11-10 18:31:06 +0100

    Added comments

--- dice.ino
+++ dice.ino
@@ -2,8 +2,10 @@ const int BUTTON = 2;
 const int START_PIN = 3;
 const int END_PIN = 9;
 
+// If this is enabled, will the dice never display the same value as the last one
 const bool LESS_RANDOM = true;
 
+// An array of all numbers and which leds should be turned on in ordner to display the numbers right
 const int NUMBERS[6][7] = {
     {LOW, LOW, LOW, HIGH, LOW, LOW, LOW},         // 1 
     {HIGH, LOW, LOW, LOW, HIGH, LOW, LOW},        // 2 
@@ -13,15 +15,14 @@ const int NUMBERS[6][7] = {
     {HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH}     // 6 
 };
 
+int lastNumber = -1;
+
 void setup() {
     pinMode(BUTTON, INPUT);
     for(int i = START_PIN; i <= END_PIN; i++) {
         pinMode(i, OUTPUT);
     }
-    Serial.begin(9600);
 }
-
-int lastNumber = -1;
 
 void loop() {
     if(digitalRead(BUTTON) != LOW) {
@@ -30,10 +31,11 @@ void loop() {
 
     int randomNumber;
     do {
-      randomNumber = random(0, 6);
-      Serial.println(randomNumber);
+        // Generate a number from 0 to 5
+        randomNumber = random(0, 6);
     } while(LESS_RANDOM && randomNumber == lastNumber);
  
+    // Display the number
     for(int i = 0; i < 7; i++) {
         digitalWrite(START_PIN + i, NUMBERS[randomNumber][i]);  
     }
