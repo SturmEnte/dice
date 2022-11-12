2e1f2d869eb24103191de728d94a7f38cbfca24e
Jonas <55847228+SturmEnte@users.noreply.github.com>
2022-11-12 23:47:07 +0100

    Changed how a the two is rendered

--- dice.ino
+++ dice.ino
@@ -8,7 +8,7 @@ const bool LESS_RANDOM = true;
 // An array of all numbers and which leds should be turned on in ordner to display the numbers right
 const int NUMBERS[6][7] = {
     {LOW, LOW, LOW, HIGH, LOW, LOW, LOW},         // 1 
-    {HIGH, LOW, LOW, LOW, HIGH, LOW, LOW},        // 2 
+    {LOW, LOW, HIGH, LOW, HIGH, LOW, LOW},        // 2 
     {LOW, LOW, HIGH, HIGH, LOW, LOW, HIGH},       // 3  
     {HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH},      // 4  
     {HIGH, LOW, HIGH, HIGH, HIGH, LOW, HIGH},     // 5 
